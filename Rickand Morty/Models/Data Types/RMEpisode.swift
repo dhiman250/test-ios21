1e92b4f53e4c21c8a67a1b079e60cabfef28cc8f
Dhiman Das <dhimandas10.appleid@gmail.com>
2024-12-29 02:55:29 +0100

    RMCharacter22

--- Rickand Morty/Models/Data Types/RMEpisode.swift
+++ Rickand Morty/Models/Data Types/RMEpisode.swift
@@ -16,3 +16,20 @@ struct RMEpisode: Codable {
     let url: String
     let created: String
 }
+//struct deb: Codable {
+//    let id: Int
+//    let name: String
+//    let air_date: String
+//    let episode: String
+//    let characters: [String]
+//    let url: String
+//    let created: String
+//}struct deb: Codable {
+//    let id: Int
+//    let name: String
+//    let air_date: String
+//    let episode: String
+//    let characters: [String]
+//    let url: String
+//    let created: String
+//}
